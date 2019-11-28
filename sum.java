49e4b5723224bc09aaa30396917842143157bd78
gabrielspassos <gabrielsantos45725@gmail.com>
2019-11-28 20:33:24 -0300

    :recycle: add codigo to sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,14 @@
 class Add {
     public static void main(String[] args) {
         int x = 3;
+        int y = 5;
+        boolean z = false;
+        int a = 2;
+        int b = 4;
         System.out.println(x - 30);
+        System.out.println(y / 5);
+        System.out.println(z);
+        System.out.println(a + 2);
+        System.out.println(b * 3);
     }
 }
